5d07987f9b1f53f57c3c00697a73be03b1dc4f25
yashV0201 <vardhan.yash0201@gmail.com>
2024-02-22 04:02:07 +0530

    js start

---
+++ JS_learn/01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 654162
+let accountEmail = "klkj@gmail"
+var accountPassword = "6546"
+accountCity = "delhi"
+let accountState;
+
+//accountId = 1 // changing constants not allowed
+accountEmail = "aa.gaml"
+accountPassword = "8908"
+accountCity = "jaj"
+
+/*
+prefer not to use var in javascript because of
+issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
