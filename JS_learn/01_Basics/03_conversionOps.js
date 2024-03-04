fa3c26924631263fd0edce3009c795aa37b0c154
yashV0201 <vardhan.yash0201@gmail.com>
2024-03-05 03:13:36 +0530

    conversion of data types

---
+++ JS_learn/01_Basics/03_conversionOps.js
@@ -0,0 +1,38 @@
+let score = '33abc'
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false =>0;
+
+let isLoggedIn = "yash";
+
+let boolIsLoggedin = Boolean(isLoggedIn)
+
+//console.log(boolIsLoggedin)
+
+// 1 => true; 0 => false;
+// "" => false           empty string
+// "yash" => ture        non-empty string
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+//************** OPERTAIONS *******************//
+
+
+
+
