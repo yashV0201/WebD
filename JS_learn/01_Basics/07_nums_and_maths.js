0f4f529bbc636dee6e399acc87bf30084ed0ef21
yashV0201 <vardhan.yash0201@gmail.com>
2024-03-05 19:50:10 +0530

    math and nums

---
+++ JS_learn/01_Basics/07_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+
+const balance = new Number(1000)
+
+// console.log(balance)
+// console.log(score);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 1123.8966
+
+// console.log(anotherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//*************** Maths ******************
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.max(3,7,1,9,10,56))
+console.log(Math.random());  // generates random values between 0 and 1
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
