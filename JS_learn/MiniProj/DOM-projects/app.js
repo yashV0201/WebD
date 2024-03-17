70f087990dd4b5cf2c195937c8da67e328a7339d
yashV0201 <vardhan.yash0201@gmail.com>
2024-03-17 18:51:08 +0530

    learned using html, css & js together

--- JS_learn/MiniProj/DOM-projects/app.js
+++ JS_learn/MiniProj/DOM-projects/app.js
@@ -3,8 +3,28 @@ function subscribe(){
 
         if(buttonElement.innerHTML==='Subscribe'){
             buttonElement.innerHTML = 'Subscribed';
+            buttonElement.classList.add('is-subscribed');
         }
         else{
             buttonElement.innerHTML = 'Subscribe';
+            buttonElement.classList.remove('is-subscribed');
         }
+}
+
+function calculateTotal(){
+    const inputElement = document.querySelector('.js-cost-input');
+    let cost = Number(inputElement.value); //this gives us a sring type value... to perform calculations we first need tp first convert it into number using Number function
+    if(cost<40){                   
+        cost += 10;
+    }
+
+    document.querySelector('.js-total-cost').innerHTML = `$${cost}`
+    
+
+}
+
+function handleCostKeyDown(event){
+    if(event.key === 'Enter'){
+        calculateTotal();
+    }
 }
