1049e8097c0c884eda68aae545914ed71e02123e
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-09 01:37:12 +0530

    CRUD operations bacics

--- Backend_Learn/ExpressDir/index.js
+++ Backend_Learn/ExpressDir/index.js
@@ -1,19 +1,30 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 // console.dir(app);
 
 let port = 3000;
 
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+})
+
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params;
+    res.render("instagram.ejs",{username});
+})
+
+app.get("/rolldice",(req,res)=>{
+    let diceVal = Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{num: diceVal});
+})
+
+
 app.listen(port,()=>{
     console.log(`app is listening to port ${port}`);
 })
 
-app.use((req,res)=>{
-    
-    console.log("request recieved");
-    let code = "<h1>Wazuupp</h1><button>hi</button>"
-    res.send(code);
-
-
-})
---
+++ Backend_Learn/REST_Class/index.js
@@ -0,0 +1,79 @@
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const{v4:uuidv4} = require("uuid");
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        id:uuidv4(),
+        username:"yash_v7",
+        content:"get shit done!"
+    },
+    {
+        id:uuidv4(),
+        username:"yash_v77",
+        content:"get shit done(2)!"
+    },
+    {
+        id:uuidv4(),
+        username:"yash_v78",
+        content:"get shit done(3)!"
+    },
+    
+]
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    let id= uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id===p.id);
+    res.render("show.ejs",{post});
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=>id === p.id);
+    post.content=newContent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id === p.id);
+    res.render("edit.ejs",{post});
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=>id !== p.id);
+    res.redirect("/posts");
+})
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+})
